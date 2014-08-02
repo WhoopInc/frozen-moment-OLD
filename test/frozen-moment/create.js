moment/moment

Add test for momentProperties and cloning

test/moment/create.js
@@ -128,6 +128,18 @@ exports.create = {
         test.done();
     },
 
+    "cloning respects moment.momentProperties" : function (test) {
+        var m = moment();
+
+        test.equal(m.clone()._special, undefined, "cloning ignores extra properties");
+        m._special = "bacon";
+        moment.momentProperties.push("_special");
+        test.equal(m.clone()._special, "bacon", "cloning respects momentProperties");
+        moment.momentProperties.pop();
+
+        test.done();
+    },
+
     "undefined" : function (test) {
         test.expect(1);
         test.ok(moment().toDate() instanceof Date, "undefined");

WhoopInc/frozen-moment-OLD

test/frozen-moment/create.js
@@ -129,6 +129,18 @@ exports.create = {
         test.done();
     },
 
+    "cloning respects frozenMoment.instanceProperties" : function (test) {
+        var m = frozenMoment.build();
+
+        test.equal(m.clone()._special, undefined, "cloning ignores extra properties");
+        m._special = "bacon";
+        frozenMoment.instanceProperties.push("_special");
+        test.equal(m.clone()._special, "bacon", "cloning respects instanceProperties");
+        frozenMoment.instanceProperties.pop();
+
+        test.done();
+    },
+
     "undefined" : function (test) {
         test.expect(1);
         test.ok(frozenMoment().toDate() instanceof Date, "undefined");