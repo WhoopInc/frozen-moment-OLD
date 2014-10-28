20338b35b662741e576304d8e792949c6c2188fa
Lucas Sanders <git@butterflyhug.com>
2014-10-27 23:36:20 -0400

    Fix frozenMoment references

--- test/frozen-moment/is_date.js
+++ test/frozen-moment/is_date.js
@@ -1,30 +1,30 @@
-var moment = require('../../moment');
+var frozenMoment = require('../../frozen-moment');
 
 exports.add = {
     setUp : function (done) {
-        moment.createFromInputFallback = function () {
-            throw new Error('input not handled by moment');
+        frozenMoment.createFromInputFallback = function () {
+            throw new Error('input not handled by frozenMoment');
         };
         done();
     },
 
     'isDate recognizes Date objects' : function (test) {
-        test.ok(moment.isDate(new Date()), 'no args (now)');
-        test.ok(moment.isDate(new Date([2014, 02, 15])), 'array args');
-        test.ok(moment.isDate(new Date('2014-03-15')), 'string args');
-        test.ok(moment.isDate(new Date('does NOT look like a date')), 'invalid date');
+        test.ok(frozenMoment.isDate(new Date()), 'no args (now)');
+        test.ok(frozenMoment.isDate(new Date([2014, 02, 15])), 'array args');
+        test.ok(frozenMoment.isDate(new Date('2014-03-15')), 'string args');
+        test.ok(frozenMoment.isDate(new Date('does NOT look like a date')), 'invalid date');
         test.done();
     },
 
     'isDate rejects non-Date objects' : function (test) {
-        test.ok(!moment.isDate(), 'nothing');
-        test.ok(!moment.isDate(undefined), 'undefined');
-        test.ok(!moment.isDate(null), 'string args');
-        test.ok(!moment.isDate(42), 'number');
-        test.ok(!moment.isDate('2014-03-15'), 'string');
-        test.ok(!moment.isDate([2014, 2, 15]), 'array');
-        test.ok(!moment.isDate({year: 2014, month: 2, day: 15}), 'object');
-        test.ok(!moment.isDate({toString: function () {
+        test.ok(!frozenMoment.isDate(), 'nothing');
+        test.ok(!frozenMoment.isDate(undefined), 'undefined');
+        test.ok(!frozenMoment.isDate(null), 'string args');
+        test.ok(!frozenMoment.isDate(42), 'number');
+        test.ok(!frozenMoment.isDate('2014-03-15'), 'string');
+        test.ok(!frozenMoment.isDate([2014, 2, 15]), 'array');
+        test.ok(!frozenMoment.isDate({year: 2014, month: 2, day: 15}), 'object');
+        test.ok(!frozenMoment.isDate({toString: function () {
             return '[object Date]';
         }}), 'lying object');
         test.done();
