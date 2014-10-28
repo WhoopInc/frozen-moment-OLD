moment/moment

Use global or this instead of window

locale/en-gb.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     return moment.defineLocale('en-gb', {

locale/he.js
@@ -10,7 +10,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     return moment.defineLocale('he', {

locale/pt.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     return moment.defineLocale('pt', {

locale/ru.js
@@ -9,7 +9,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     function plural(word, num) {

locale/sq.js
@@ -10,7 +10,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     return moment.defineLocale('sq', {

locale/ta.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     /*var symbolMap = {

locale/th.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     return moment.defineLocale('th', {

locale/zh-cn.js
@@ -9,7 +9,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../moment')); // Node
     } else {
-        factory(window.moment); // Browser global
+        factory((typeof global !== undefined ? global : this).moment); // Browser global
     }
 }(function (moment) {
     return moment.defineLocale('zh-cn', {

WhoopInc/frozen-moment-OLD

locale/en-gb.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     return frozenMoment.defineLocale('en-gb', {

locale/he.js
@@ -10,7 +10,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     return frozenMoment.defineLocale('he', {

locale/pt.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     return frozenMoment.defineLocale('pt', {

locale/ru.js
@@ -9,7 +9,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     function plural(word, num) {

locale/sq.js
@@ -10,7 +10,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     return frozenMoment.defineLocale('sq', {

locale/ta.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     /*var symbolMap = {

locale/th.js
@@ -8,7 +8,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     return frozenMoment.defineLocale('th', {

locale/zh-cn.js
@@ -9,7 +9,7 @@
     } else if (typeof exports === 'object') {
         module.exports = factory(require('../frozen-moment')); // Node
     } else {
-        factory(window.frozenMoment); // Browser global
+        factory((typeof global !== undefined ? global : this).frozenMoment); // Browser global
     }
 }(function (frozenMoment) {
     return frozenMoment.defineLocale('zh-cn', {